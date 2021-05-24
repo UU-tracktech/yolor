23b7644b04ed062ec5cfec768e53f74ba65038a0
Jaimy Kwa <jaimykwa@gmail.com>
2021-05-24 12:37:58 +0200

    FIX Added more specific CPU request

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -42,7 +42,9 @@ def init_torch_seeds(seed=0):
 def select_device(device='', batch_size=None):
     # device = 'cpu' or '0' or '0,1,2,3'
     cpu_request = device.lower() == 'cpu'
-    if device and not cpu_request:  # if device requested other than 'cpu'
+    if cpu_request:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Force torch.cuda.is_available() = False
+    elif device:  # if device requested other than 'cpu'
         os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
         assert torch.cuda.is_available(), 'CUDA unavailable, invalid device %s requested' % device  # check availablity
 
